274b0693be3cfe3fdf28dd58b07ef23da0b73378
ricardora01 <ricardo.ra@aguascalientes.tecnm.mx>
2024-10-08 10:22:30 -0600

    changed login form

--- PHP/login2.php
+++ PHP/login2.php
@@ -1,13 +1,13 @@
 <?php
 
-$user = $_POST ["user"];
+$user = $_POST ["email"];
 $password = $_POST ["password"];
 
-if($user == 'ricardo' && $password == "123abc."){
-    header (header: 'Location: http://localhost/attendance/attendance/home.php');
+if($user == 'admin' && $password == "123abc."){
+    header (header: 'Location: ../home.php');
 }else{
     echo '<script language="javascript">alert("Datos incorrectos") </script>';
-    header ("Location: index2.php");
+    header ("Location: ../index.php");
 
 }
 
