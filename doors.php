11cef89a4a96b5e200780da2a96e010626682cff
ricardora01 <ricardo.ra@aguascalientes.tecnm.mx>
2024-11-05 10:34:18 -0600

    Feat :sparkles: added datatable pagination and filters

--- doors.php
+++ doors.php
@@ -23,6 +23,9 @@ $id = $_GET ["id"] ??  null;
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
+    <!-- Custom styles for this page -->
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
 </head>
 
 <body id="page-top">
@@ -198,6 +201,13 @@ $id = $_GET ["id"] ??  null;
 
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
 
     <script type="text/javascript">
         $(window).on('load', function() {
