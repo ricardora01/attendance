8469aa0349d7cdb9c401f4a36167bb222c7fc99d
ricardora01 <ricardo.ra@aguascalientes.tecnm.mx>
2024-11-06 10:29:09 -0600

    datatable

--- PHP/auth/access.php
+++ PHP/auth/access.php
@@ -4,12 +4,12 @@ require('PHP/database.php');
 
 $sql = "SELECT accesslog.AccessLogId, accesslog.VisitorId, accesslog.DateIn, accesslog.TimeIn, door.DoorName AS Door, 
 department.Name AS department, accesslog.PersonToVisiti, accesslog.VisitMotive, accesslog.GuardId, accesslog.DateOut, accesslog.TimeOut 
-FROM accesslog JOIN door ON accesslog.DoorId = door.DoorName JOIN department ON accesslog.DepartmentToVisitId = department.Name;";
+FROM accesslog JOIN door ON accesslog.DoorId = door.DoorId JOIN department ON accesslog.DepartmentToVisitId = department.Department;";
 
 $statement = $conn->query($sql);
 
 $access_list = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-//echo json_encode($access_list);
+///echo json_encode($access_list);
 
 ?>
--- accesslog.php
+++ accesslog.php
@@ -23,6 +23,9 @@ $id = $_GET ["id"] ??  null;
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
+     <!-- Custom styles for this page -->
+     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
 </head>
 
 <body id="page-top">
@@ -73,6 +76,7 @@ $id = $_GET ["id"] ??  null;
                                             <th>ID guardia</th>
                                             <th>Fecha de salida</th>
                                             <th>Hora de salida</th>
+                                            <th>Acciones</th>
                                             
                                         </tr>
                                     </thead>
@@ -89,13 +93,14 @@ $id = $_GET ["id"] ??  null;
                                             <th>ID guardia</th>
                                             <th>Fecha de salida</th>
                                             <th>Hora de salida</th>
+                                            <th>Acciones</th>
                                             
                                         </tr>
                                     </tfoot>
                                     <tbody>
                                         <?php
-                                            include "php/auth/access.php";
-                                            //echo json_encode($door_list);
+                                            include "PHP/auth/access.php";
+                                            //echo json_encode($access_list);
 
                                                 foreach ($access_list as $access) {
                                                     echo "
@@ -111,7 +116,14 @@ $id = $_GET ["id"] ??  null;
                                                         <td>". $access['GuardId'] ."</td>
                                                         <td>". $access['DateOut'] ."</td>
                                                         <td>". $access['TimeOut'] ."</td>
-                                                        
+                                                        <td>
+                                                            <a href='#' class='btn btn-success btn-icon-split btn-sm'>
+                                                                <span class='icon text-white-50'>
+                                                                    <i class='fas fa-check'></i>
+                                                                </span>
+                                                                <span class='text'>Registrar salida</span>
+                                                            </a>
+                                                        </td>
                                                     </tr>
                                                     ";
                                                 }
@@ -221,6 +233,13 @@ $id = $_GET ["id"] ??  null;
             $('#exampleModal2').modal('show');
         });
     </script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
 
 </body>
 
