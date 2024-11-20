81897bb81db9c1013c1d02b43afcc66e60898052
IsidroSZ <24153288@aguascalientes.tecnm.mx>
2024-11-20 02:12:01 -0600

    Feat ✨Added data in stats.php

--- stats.php
+++ stats.php
@@ -230,6 +230,138 @@
                         
                     </div>
 
+                    <div class="row">
+
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Visitantes en el dia</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"> <?php
+                                                                                                     try {
+                                                                                                        require('PHP/database.php');
+                                                                                                        $sql = "SELECT COUNT(AccessLogId) as Access1 FROM `accesslog`;";
+                                                                                                        $statement = $conn->query($sql);
+                                                                                                        $Access = $statement->fetchAll(PDO::FETCH_ASSOC);
+                                                                                                        
+                                                                                                      //  echo json_encode($Access);
+                                                                                                    } catch(PDOException $e) {
+                                                                                                        die();
+                                                                                                        echo "Error: " . $e->getMessage();
+                                                                                                    }
+                                                                                                    //$Access_str = json_encode($Access);
+                                                                                                    echo $Access[0]['Access1'];
+                                                                                                    ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Earnings (Annual) Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Visitantes dentro del Instituto Tecnológico de Aguascalientes</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php
+                                                                                                    
+                                                                                                            try {
+                                                                                                                require('PHP/database.php');
+                                                                                                               // $sql = "SELECT COUNT(AccessLogId) as Access1 FROM `accesslog`;";
+                                                                                                                $sql = "SELECT COUNT(AccessLogId) as AccessIn FROM `accesslog` WHERE TimeOut IS NULL;";
+                                                                                                                $statement = $conn->query($sql);
+                                                                                                                $AccessI = $statement->fetchAll(PDO::FETCH_ASSOC);
+                                                                                                                
+                                                                                                                //  echo json_encode($Access);
+                                                                                                            }   catch(PDOException $e) {
+                                                                                                                die();
+                                                                                                                echo "Error: " . $e->getMessage();
+                                                                                                            }
+                                                                                                                //$Access_str = json_encode($Access);
+                                                                                                                echo $AccessI[0]['AccessIn'];
+                                                                                                    ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas  fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Tasks Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Visitantes que han salido
+                                            </div>
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php
+                                                                                                    
+                                                                                                                try {
+                                                                                                                    require('PHP/database.php');
+                                                                                                                // $sql = "SELECT COUNT(AccessLogId) as Access1 FROM `accesslog`;";
+                                                                                                                    $sql = "SELECT COUNT(AccessLogId) as AccessOut FROM `accesslog` WHERE TimeOut IS NOT NULL;";
+                                                                                                                    $statement = $conn->query($sql);
+                                                                                                                    $AccessO = $statement->fetchAll(PDO::FETCH_ASSOC);
+                                                                                                                    
+                                                                                                                    //  echo json_encode($Access);
+                                                                                                                }   catch(PDOException $e) {
+                                                                                                                    die();
+                                                                                                                    echo "Error: " . $e->getMessage();
+                                                                                                                }
+                                                                                                                    //$Access_str = json_encode($Access);
+                                                                                                                    echo $AccessO[0]['AccessOut'];
+                                                                                                             ?></div>
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Pending Requests Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                Fecha</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php
+                                                                                                date_default_timezone_set("America/Mexico_City");
+                                                                                                $DateIn = date("Y-m-d");
+                                                                                                echo $DateIn;
+                                                                                                ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                
+                    </div>
+
 
 
                 </div>
