8b6613d427bc6035cbd54ed9e9ec99e40e763868
ricardora01 <ricardo.ra@aguascalientes.tecnm.mx>
2024-11-26 10:57:59 -0600

    Feat :sparkles: added login with database

--- PHP/login2.php
+++ PHP/login2.php
@@ -1,14 +1,39 @@
 <?php
+require('database.php');
+$user = $_POST["user"];
+$password = $_POST["password"];
+$password = hash('sha256', $password);
+//$password = hash('sha256', $password);
 
-$user = $_POST ["email"];
-$password = $_POST ["password"];
+try {
+    $sql = "SELECT FirstName, LastName, EmployeeId FROM guard 
+    WHERE EmployeeId = :user AND Password = :password";
+    $statement = $conn->prepare($sql);
+    $statement->bindParam(':user', $user);
+    $statement->bindParam(':password', $password);
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+    if(count($result) == 1 ){
+        header (header: 'Location: ../presentation.php');
+    } else {
+        header (header: 'Location: ../index.php?error=1');
+    }
+
+    echo json_encode($result);
+} catch(PDOException $e) {
+    die();
+    echo "Error: " . $e->getMessage();
+}
+
+
+/*
 if($user == 'admin@localhost.com' && $password == "123abc."){
     header (header: 'Location: ../home.php');
 }else{
     echo '<script language="javascript">alert("Datos incorrectos") </script>';
     header ("Location: ../index.php");
 
-}
+}*/
 
 ?>
